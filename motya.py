190c98bc488baae4f44083de5c86ba43d29c9d23
nm17 <dannevergame@gmail.com>
2020-04-14 00:58:12 +0400

    Add classes and stuff

--- motya.py
+++ motya.py
@@ -1,78 +1,37 @@
 import os
 import random
+import time
 
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
 
-TEMPLATE_FILENAME = "template.jpg"
-PHRASES_FILENAME = "phrases.txt"
-IMAGES_DIRECTORY = "images"
-EXTENSIONS = [".jpg", ".png"]
-
-RESULT_FILENAME = "result.png"
-
-UPPER_FONT = "times.ttf"
-UPPER_SIZE = 45
-UPPER_FONT_Y = 390
-
-TEMPLATE_WIDTH = 574
-TEMPLATE_HEIGHT = 522
-TEMPLATE_COORDS = (75, 45, 499, 373)
-PADDING = 10
-
-
-def is_valid_extension(filename):
-    for extension in EXTENSIONS:
-        if filename.endswith(extension):
-            return True
-    return False
-
-
-def get_random_image():
-    file_list = []
-    for dirpath, dirnames, filenames in os.walk(IMAGES_DIRECTORY):
-        if TEMPLATE_FILENAME in filenames:
-            filenames.remove(TEMPLATE_FILENAME)
-        for filename in [f for f in filenames if is_valid_extension(f)]:
-            file_list.append(os.path.join(IMAGES_DIRECTORY, filename))
-    return Image.open(random.choice(file_list))
-
-
-def get_random_phrase():
-    with open(PHRASES_FILENAME) as file:
-        content = file.read().splitlines()
-    return random.choice(content)
-
-
-def draw_x_axis_centered_text(image, text, font, size, pos_y):
-    draw = ImageDraw.Draw(image)
-    text_font = ImageFont.truetype(font, size)
-    text_width = text_font.getsize(text)[0]
-
-    while text_width >= TEMPLATE_WIDTH - PADDING * 2:
-        text_font = ImageFont.truetype(font, size)
-        text_width = text_font.getsize(text)[0]
-        size -= 1
-
-    draw.text(((TEMPLATE_WIDTH - text_width) / 2, pos_y), text, font=text_font)
-
-
-def get_size_from_area(area):
-    return area[2] - area[0], area[3] - area[1]
-
-
-def make_image():
-    frame = Image.open(TEMPLATE_FILENAME)
-    demot = get_random_image()
-    demot = demot.resize(get_size_from_area(TEMPLATE_COORDS), Image.ANTIALIAS)
-    frame.paste(demot, TEMPLATE_COORDS)
-
-    draw_x_axis_centered_text(
-        frame, get_random_phrase(), UPPER_FONT, UPPER_SIZE, UPPER_FONT_Y
-    )
-
-    frame.save(RESULT_FILENAME)
-    frame.show()
+from motya.motya import Motya
+from motya.template import Template, Frame
 
 
 if __name__ == "__main__":
-    make_image()
+    seed = time.time()
+    print(f"Seed: {seed}")
+    random.seed(seed)
+
+    main_template = Template(
+        image=Image.open("template.jpg"),
+        width=574,
+        height=522,
+        frame=Frame(x_start=75, y_start=45, x_end=499, y_end=373),
+        padding=10,
+    )
+
+    file_list = []
+    for dirpath, dirnames, filenames in os.walk("images/"):
+        for filename in [f for f in filenames if f.endswith(("png", "jpg", "jpeg"))]:
+            file_list.append(Image.open(os.path.join("images/", filename)))
+
+    motya = Motya(
+        template=main_template,
+        phrases=list(open("phrases.txt", encoding="utf-8").readlines()),
+        images=file_list,
+    )
+
+    output = motya.generate()
+    output.save("result.png")
+    output.show()
